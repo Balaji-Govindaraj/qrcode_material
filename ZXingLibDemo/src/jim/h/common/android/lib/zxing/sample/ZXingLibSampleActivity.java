a4123aae88136dbde389d4680f22a1b96dfb9c69
Balaji-Govindaraj <baluthegreat2@gmail.com>
2016-03-25 21:36:28 +0530

    Update ZXingLibSampleActivity.java

--- ZXingLibDemo/src/jim/h/common/android/lib/zxing/sample/ZXingLibSampleActivity.java
+++ ZXingLibDemo/src/jim/h/common/android/lib/zxing/sample/ZXingLibSampleActivity.java
@@ -1,28 +1,30 @@
 package jim.h.common.android.lib.zxing.sample;
 
-import jim.h.common.android.lib.zxing.config.ZXingLibConfig;
-import jim.h.common.android.lib.zxing.integrator.IntentIntegrator;
-import jim.h.common.android.lib.zxing.integrator.IntentResult;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-public class  ZXingLibSampleActivity extends AppCompatActivity {
+import jim.h.common.android.lib.zxing.config.ZXingLibConfig;
+import jim.h.common.android.lib.zxing.integrator.IntentIntegrator;
+import jim.h.common.android.lib.zxing.integrator.IntentResult;
 
+public class  ZXingLibSampleActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+    private TextView txtScanResult;
+    private ZXingLibConfig zxingLibConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+
+        txtScanResult = (TextView) findViewById(R.id.sid);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,10 +32,32 @@ public class  ZXingLibSampleActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                IntentIntegrator.initiateScan(ZXingLibSampleActivity.this, zxingLibConfig);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case IntentIntegrator.REQUEST_CODE: // 扫描结果
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode,
+                        resultCode, data);
+                if (scanResult == null) {
+                    return;
+                }
+                final String result = scanResult.getContents();
+                if (result != null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtScanResult.setText(result);
+                        }
+                    });
+                }
+                break;
+            default:
+        }
     }
 
     @Override
